7b6a793d03204fdf5002eda17f8f7b28aa90feaa
LucaBarrella <lucabarrella@icloud.com>
2024-05-19 15:40:17 +0200

    altri test aggiunti

--- exercise2/zmytest/test.cpp
+++ exercise2/zmytest/test.cpp
@@ -353,6 +353,19 @@ void mytest() {
 
       //Todo Se ho genio (e sopratutto tempo) faccio qualche altro test.
 
+      lasd::BTBreadthIterator<int> itr11(itr10);
+      if (itr11 == itr10){ std::cout << "Correct!" << std::endl;}
+      else if (itr11 != itr10){std::cout << "Error!" << std::endl;}
+
+      lasd::BTBreadthIterator<int> itr12(std::move(itr11));
+
+      lasd::BTBreadthIterator<int> itr13(btlnk);
+      itr13 = itr12;
+      if (itr13 == itr12) {std::cout << "Correct!" << std::endl;}
+      else if (itr13 != itr12) {std::cout << "Error!" << std::endl;} 
+
+      //Todo Se ho genio (e sopratutto tempo) faccio qualche altro test.
+
     } catch (...) {
       numTestexecuted++; numError++;
       cout << endl << "Unmanaged error! " << endl;
@@ -408,24 +421,173 @@ void mytest() {
       TraverseBreadth(testnum, testerr, btlnk6, true, &TraversePrint<int>);
       Fold(testnum, testerr, btlnk6, true, &FoldAdd<int>, 0, 0);
 
+
+
+      // todo spostastare in btvec
       lasd::BinaryTreeVec<int> btvec;
       btvec = btlnk6;
       EqualBT(testnum, testerr, btvec, btlnk6);
       Traverse(testnum, testerr, btvec, true, &TraversePrint<int>);
 
+      
+
+
 
       //todo Testare exists e vedere se modifica i dati.
 
 
+      //todo Exists
+      //todo Chiamo ogni funzione almeno una volta
+      //todo in bst per esempio prende in input minimo, massimo ecc. e controlla che sia giusto
+      //todo E chiamo quel test su un bst vuoto, uno degenere e uno random
+
+      Empty(testnum, testerr, btlnk7, true);
+      Size(testnum, testerr, btlnk7, true, 0);
+      Exists(testnum, testerr, btlnk7, false, 1);
+      btlnk7 = l1;
+      Exists(testnum, testerr, btlnk7, true, 1);
+      Exists(testnum, testerr, btlnk7, true, 16);
+      Exists(testnum, testerr, btlnk7, false, 17);
+      Exists(testnum, testerr, btlnk7, false, 0);
+      Exists(testnum, testerr, btlnk7, false, -1);
+      Exists(testnum, testerr, btlnk7, false, 100);
+      Exists(testnum, testerr, btlnk7, true, 8);
+      Exists(testnum, testerr, btlnk7, false, 104);
+      btlnk7.Root().LeftChild().LeftChild().Element() = int(104);
+      Exists(testnum, testerr, btlnk7, true, 104);
 
 
+      if (!btlnk7.Root().LeftChild().LeftChild().IsLeaf()) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      testnum++;
 
-
-
+      lasd::Vector<int> vectLeaf(1);
+      vectLeaf[0] = 104;
+      lasd::BinaryTreeLnk<int> btlnk8;
+      btlnk8 = vectLeaf;
+      Exists(testnum, testerr, btlnk8, true, 104);
+      Exists(testnum, testerr, btlnk8, false, 105);
+      if (btlnk8.Root().Element() == 104) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      if (!btlnk8.Root().HasLeftChild()) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      if (!btlnk8.Root().HasRightChild()) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      if (btlnk8.Root().IsLeaf()) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      testnum+=4;
+      lasd::Vector<int> vec2(2);
+      vec2[0] = 104;
+      vec2[1] = 105;
+      btlnk8 = vec2;
+      Exists(testnum, testerr, btlnk8, true, 104);
+      Exists(testnum, testerr, btlnk8, true, 105);
+      Exists(testnum, testerr, btlnk8, false, 106);
+      if (btlnk8.Root().Element() == 104) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      if (btlnk8.Root().LeftChild().Element() == 105) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      testnum+=2;
+      
     } catch (...) {
       numTestexecuted++; numError++;
       cout << endl << "Unmanaged error! " << endl;
     }
   }
-  void binaryTreeVectorTest(){} //TODO
-  void binarySearchTreeLinkedTest(){} //TODO
+  void binaryTreeVectorTest(){
+    lasd::Vector<int> v1(16);
+    for (ulong i = 0; i < v1.Size(); i++) { 
+      v1[i] = i + 1;
+    }
+    std::cout << "Testing BinaryTreeVec<int>" << std::endl;
+    unsigned int testnum = 0;
+    unsigned int testerr = 0;
+    lasd::BinaryTreeVec<int> btvec1;
+
+    try
+    {
+      Empty(testnum, testerr, btvec1, true);
+      Size(testnum, testerr, btvec1, true, 0);
+      btvec1 = v1;
+      Empty(testnum, testerr, btvec1, false);
+      Size(testnum, testerr, btvec1, true, 16);
+      lasd::BinaryTreeVec<int> btvec2(btvec1);
+      EqualBT(testnum, testerr, btvec1, btvec2);
+      lasd::BinaryTreeVec<int> btvec3(std::move(btvec2));
+      NonEqualBT(testnum, testerr, btvec1, btvec2);
+      EqualBT(testnum, testerr, btvec1, btvec3);
+      lasd::BinaryTreeVec<int> btvec4;
+      lasd::BinaryTreeVec<int> btvec5;
+      EqualBT(testnum, testerr, btvec4, btvec5);
+      btvec4 = btvec5;
+      Traverse(testnum, testerr, btvec4, true, &TraversePrint<int>);
+      TraversePreOrder(testnum, testerr, btvec4, true, &TraversePrint<int>);
+      TraverseInOrder(testnum, testerr, btvec4, true, &TraversePrint<int>);
+      TraversePostOrder(testnum, testerr, btvec4, true, &TraversePrint<int>);
+      TraverseBreadth(testnum, testerr, btvec4, true, &TraversePrint<int>);
+      btvec4 = std::move(btvec5);
+      EqualBT(testnum, testerr, btvec4, btvec5);
+      lasd::BinaryTreeVec<int> btvec6 (v1);
+      Exists(testnum, testerr, btvec6, true, 1);
+      Exists(testnum, testerr, btvec6, true, 16);
+      Exists(testnum, testerr, btvec6, false, 17);
+      Exists(testnum, testerr, btvec6, false, 0);
+      Exists(testnum, testerr, btvec6, false, -1);
+      Exists(testnum, testerr, btvec6, true, 8);
+      lasd::BinaryTreeVec<int> btvec7;
+      Empty(testnum, testerr, btvec7, true);
+      Size(testnum, testerr, btvec7, true, 0);
+      Exists(testnum, testerr, btvec7, false, 1);
+      Exists(testnum, testerr, btvec7, false, 0);
+      Fold(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 0);
+      FoldPreOrder(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 0);
+      FoldInOrder(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 0);
+      FoldPostOrder(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 0);
+      FoldBreadth(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 0);
+      btvec7 = v1;
+      Fold(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 136);
+      FoldPreOrder(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 136);
+      FoldInOrder(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 136);
+      FoldPostOrder(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 136);
+      FoldBreadth(testnum, testerr, btvec7, true, &FoldAdd<int>, 0, 136);
+
+      if (btvec7.Root().Element() == 1) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      if (btvec7.Root().LeftChild().Element() == 2) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      if (btvec7.Root().RightChild().Element() == 3) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      if (!btvec7.Root().LeftChild().LeftChild().IsLeaf()) {std::cout << "Correct!" << std::endl;}
+      else {std::cout << "Error!" << std::endl;}
+      testnum+=4;
+
+      lasd::BinaryTreeVec<int> btvec8 (v1);
+      lasd::BinaryTreeVec<int> btvec9 (v1);
+      EqualBT(testnum, testerr, btvec8, btvec9);
+      btvec8.Clear();
+      NonEqualBT(testnum, testerr, btvec8, btvec9);
+      btvec8 = v1;
+      EqualBT(testnum, testerr, btvec8, btvec9);
+      btvec8 = std::move(btvec9);
+      EqualBT(testnum, testerr, btvec8, btvec9); //! Perché nel move assigment io faccio lo swap dei dati, quindi i due oggetti sono uguali
+
+      lasd::BinaryTreeVec<int> btvec10 (btvec8);
+      btvec8 = std::move(btvec8);
+      EqualBT(testnum, testerr, btvec8, btvec10);
+
+
+      //todo inserire altri test se ho tempo e voglia 
+
+      
+
+    } catch (...) {
+      numTestexecuted++; numError++;
+      cout << endl << "Unmanaged error! " << endl;
+    }
+    
+    
+  }
+  void binarySearchTreeLinkedTest(){
+    //todo Fare ora!
+
+  }

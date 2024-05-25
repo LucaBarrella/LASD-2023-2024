0200ac29e857da4aa93aaf94076fd16476bda484
LucaBarrella <lucabarrella@icloud.com>
2024-05-25 19:12:16 +0200

    Open Adressable, header file terminated

--- exercise3/hashtable/opnadr/htopnadr.cpp
+++ exercise3/hashtable/opnadr/htopnadr.cpp
@@ -3,7 +3,104 @@ namespace lasd {
 
 /* ************************************************************************** */
 
-// ...
+// Default constructor
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr() {
+    table = new Data[tableSize] {};
+    tableStatus = new Status[tableSize] {};
+}
+
+// Specific constructor
+
+// Constructor of a given size
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr(const unsigned long size) {
+    table = new Data[size] {};
+    tableStatus = new Status[size] {};
+    tableSize = size; //todo removable, maybe
+}
+
+// Constructor obtained from a TraversableContainer
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr(const TraversableContainer<Data>& container) : HashTableOpnAdr(container.Size()*2) { //todo why *2?
+    InsertAll(container);
+}
+
+// Constructor obtained from a TraversableContainer with specification of the size
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr(const unsigned long size, const TraversableContainer<Data>& container) : HashTableOpnAdr(size) {
+    InsertAll(container);
+}
+
+// Constructor obtained from a MappableContainer
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr(MappableContainer<Data>&& container) : HashTableOpnAdr(container.Size()*2) {
+    InsertAll(std::move(container));
+}
+
+// Constructor obtained from a MappableContainer with specification of the size
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr(const unsigned long size, MappableContainer<Data>&& container) : HashTableOpnAdr(size) {
+    InsertAll(std::move(container));
+}
+
+// Copy constructor
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr(const HashTableOpnAdr<Data>& ht) : HashTableOpnAdr(ht.tableSize) {
+    table = new Data[tableSize] {};
+    tableStatus = new Status[tableSize] {};
+    for(unsigned long i = 0; i < tableSize; i++){
+        table[i] = ht.table[i];
+        tableStatus[i] = ht.tableStatus[i];
+    }
+}
+
+// Move constructor
+template <typename Data>
+HashTableOpnAdr<Data>::HashTableOpnAdr(HashTableOpnAdr<Data>&& ht) noexcept {
+    std::swap(table, ht.table);
+    std::swap(tableStatus, ht.tableStatus);
+    // std::swap(tableSize, ht.tableSize); //todo removable, maybe
+}
+
+// Destructor
+template <typename Data>
+HashTableOpnAdr<Data>::~HashTableOpnAdr() {
+    delete[] table;
+    delete[] tableStatus;
+}
+
+// Copy assignment
+template <typename Data>
+HashTableOpnAdr<Data>& HashTableOpnAdr<Data>::operator=(const HashTableOpnAdr<Data>& ht) {
+    if(this != &ht){
+        HashTableOpnAdr<Data> temp(ht);
+        std::swap(table, temp.table);
+        std::swap(tableStatus, temp.tableStatus);
+    }
+    return *this;
+}
+
+// Move assignment
+template <typename Data>
+HashTableOpnAdr<Data>& HashTableOpnAdr<Data>::operator=(HashTableOpnAdr<Data>&& ht) noexcept {
+    //! It's the fucking same as the move constructor
+    std::swap(table, ht.table);
+    std::swap(tableStatus, ht.tableStatus);
+    // std::swap(tableSize, ht.tableSize); //todo removable, maybe
+    return *this;
+}
+
+// Comparison Operator
+
+// Equality Operator
+template <typename Data>
+
+
+
+
+
+
 
 /* ************************************************************************** */
 
--- exercise3/hashtable/opnadr/htopnadr.hpp
+++ exercise3/hashtable/opnadr/htopnadr.hpp
@@ -14,16 +14,23 @@ namespace lasd {
 /* ************************************************************************** */
 
 template <typename Data>
-class HashTableOpnAdr {
+class HashTableOpnAdr : virtual public HashTable<Data>{
   // Must extend HashTable<Data>
 
 private:
 
   // ...
+  enum Status {EMPTY, OCCUPIED, DELETED};
 
 protected:
 
   // using HashTable<Data>::???;
+  using HashTable<Data>::size;
+  using HashTable<Data>::HashKey;
+  using HashTable<Data>::tableSize; //todo remove and use a macro
+
+  Data* table = nullptr;
+  Status* tableStatus = nullptr;
 
   // ...
 
@@ -31,77 +38,110 @@ public:
 
   // Default constructor
   // HashTableOpnAdr() specifiers;
+  HashTableOpnAdr() = default;
 
   /* ************************************************************************ */
 
   // Specific constructors
   // HashTableOpnAdr(argument) specifiers; // A hash table of a given size
+  HashTableOpnAdr(unsigned long size);
+
   // HashTableOpnAdr(argument) specifiers; // A hash table obtained from a TraversableContainer
+  HashTableOpnAdr(const TraversableContainer<Data>& container);
+
   // HashTableOpnAdr(argument) specifiers; // A hash table of a given size obtained from a TraversableContainer
+  HashTableOpnAdr(unsigned long size, const TraversableContainer<Data>& container);
+
   // HashTableOpnAdr(argument) specifiers; // A hash table obtained from a MappableContainer
+  HashTableOpnAdr(MappableContainer<Data>&& container);
+
   // HashTableOpnAdr(argument) specifiers; // A hash table of a given size obtained from a MappableContainer
+  HashTableOpnAdr(unsigned long size, MappableContainer<Data>&& container);
 
   /* ************************************************************************ */
 
   // Copy constructor
   // HashTableOpnAdr(argument) specifiers;
+  HashTableOpnAdr(const HashTableOpnAdr<Data>& ht);
 
   // Move constructor
   // HashTableOpnAdr(argument) specifiers;
+  HashTableOpnAdr(HashTableOpnAdr<Data>&& ht) noexcept;
 
   /* ************************************************************************ */
 
   // Destructor
   // ~HashTableOpnAdr() specifiers;
+  ~HashTableOpnAdr(); //todo controllare!
 
   /* ************************************************************************ */
 
   // Copy assignment
   // type operator=(argument) specifiers;
+  HashTableOpnAdr<Data>& operator=(const HashTableOpnAdr<Data>& ht);
 
   // Move assignment
   // type operator=(argument) specifiers;
+  HashTableOpnAdr<Data>& operator=(HashTableOpnAdr<Data>&& ht) noexcept;
 
   /* ************************************************************************ */
 
   // Comparison operators
   // type operator==(argument) specifiers;
+  inline bool operator==(const HashTableOpnAdr<Data>& ht) const noexcept;
+
   // type operator!=(argument) specifiers;
+  inline bool operator!=(const HashTableOpnAdr<Data>& ht) const noexcept;
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from DictionaryContainer)
 
   // type Insert(argument) specifiers; // Override DictionaryContainer member (Copy of the value)
+  inline bool Insert(const Data& value) override;
+
   // type Insert(argument) specifiers; // Override DictionaryContainer member (Move of the value)
+  inline bool Insert(Data&& value) override;
+
   // type Remove(argument) specifiers; // Override DictionaryContainer member
+  inline bool Remove(const KeyType& key) override;
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from TestableContainer)
 
   // type Exists(argument) specifiers; // Override TestableContainer member
+  inline bool Exists(const KeyType& key) const noexcept override;
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from ResizableContainer)
 
   // type Resize(argument) specifiers; // Resize the hashtable to a given size
+  void Resize(unsigned long newSize);
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from ClearableContainer)
 
   // type Clear() specifiers; // Override Container member
+  void Clear() override;
 
 protected:
 
   // Auxiliary member functions
 
   // type HashKey(argument) specifiers;
+  unsigned long HashKey(const Data&, unsigned long) const noexcept;
+
   // type Find(argument) specifiers;
+  bool Find(unsigned long&, unsigned long&, const Data&) const noexcept;
+
   // type FindEmpty(argument) specifiers;
+  unsigned long FindEmpty(const Data&, unsigned long&) const noexcept;
+
   // type Remove(argument) specifiers;
+  bool Remove(unsigned long&, const Data&) noexcept;
 
 };
 
